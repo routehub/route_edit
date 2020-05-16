50623ed06403f1227e89a6d9c17e90aa7b3e1a75
ctyo <ctyo@live.jp>
2020-05-16 17:06:33 +0900

    編集の1画面目実装

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', redirectTo: '/edit' }, // ブラウザキャッシュ対策
+  { path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule) },
   { path: 'layerselect', loadChildren: () => import('./layerselect/layerselect.module').then((m) => m.LayerselectPageModule) },
   { path: 'edit', loadChildren: () => import('./edit/edit.module').then((m) => m.EditPageModule) },
